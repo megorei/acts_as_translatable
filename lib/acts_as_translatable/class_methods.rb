36606697a89afbe5cd87c6732d0e035e415910b8
exsemt <exsemt@gmail.com>
2013-07-10 13:31:11 +0200

    change to update content (not to delete then create new)

--- lib/acts_as_translatable/class_methods.rb
+++ lib/acts_as_translatable/class_methods.rb
@@ -2,34 +2,35 @@ module ActsAsTranslatable
   module ClassMethods
     def acts_as_translatable_on(*fields)
       after_initialize :translations
-      after_save :save_translations
       has_many :record_translations, :foreign_key => :translatable_id, :conditions => { :translatable_type => name}, :dependent => :destroy
       default_scope :include => :record_translations
-      
+
+      accepts_nested_attributes_for :record_translations
+
       # loop through fields to define methods such as "name" and "description"
       fields.each do |field|
         define_method "#{field}" do
           get_field_content(I18n.locale, field)
         end
-        
+
         define_method "#{field}?" do
           !send("#{field}").blank?
         end
-        
+
         define_method "#{field}=" do |content|
           set_field_content(I18n.locale, field, content)
         end
-        
+
         # loop through fields to define methods such as "name_en" and "name_es"
         I18n.available_locales.each do |locale|
           define_method "#{field}_#{locale}" do
             get_field_content(locale, field)
           end
-          
+
           define_method "#{field}_#{locale}?" do
             !send("#{field}_#{locale}").blank?
           end
-          
+
           define_method "#{field}_#{locale}=" do |content|
             set_field_content(locale, field, content)
           end
@@ -41,29 +42,16 @@ module ActsAsTranslatable
         unless @translations
           @translations = {}
           I18n.available_locales.each do |locale|
-            @translations[locale] ||= {}
+            @translations[locale] ||= []
           end
           record_translations.each do |translation|
-            @translations[translation.locale.to_sym] ||= {}
-            @translations[translation.locale.to_sym][translation.translatable_field.to_sym] = translation.content
+            @translations[translation.locale.to_sym] ||= []
+            @translations[translation.locale.to_sym] << translation
           end
         end
         @translations
       end
 
-      define_method :save_translations do
-        # delete all previous translations of this record
-        record_translations.destroy_all
-        
-        # loop through updated translations
-        translations.each_pair do |locale, fields|
-          fields.each_pair do |field, content|
-            # create translation record
-            record_translations.create :translatable_field => field, :locale => locale.to_s, :content => content unless content.blank?
-          end
-        end
-      end
-      
       define_method :get_field_content do |locale, field|
         # get I18n fallbacks
         if self.class.enable_locale_fallbacks && I18n.respond_to?(:fallbacks)
@@ -71,13 +59,12 @@ module ActsAsTranslatable
         else
           locales = [locale]
         end
-        
+
         # content default
         content = nil
-        
-        # fallbacks
         locales.each do |l|
-          if content = translations[l][field]
+          if (t = translations[l.to_sym].to_a.find{|t| t.translatable_field.to_s == field.to_s }).present?
+            content = t.content
             break
           end
         end
@@ -88,7 +75,15 @@ module ActsAsTranslatable
 
       define_method :set_field_content do |locale, field, content|
         # set field content
-        translations[locale.to_sym][field] = content
+        if (t = translations[locale.to_sym].to_a.find{|t| t.translatable_field.to_s == field.to_s }).present?
+          t.content = content
+        else
+          translations[locale.to_sym] << self.record_translations.build(
+            :translatable_field => field,
+            :locale             => locale.to_sym,
+            :content            => content
+          )
+        end
       end
     end
 
