d0d2ae1d1243cc0740d183903ccafb22c2439def
Andre Winschu <aw@megorei.com>
2014-05-23 16:17:39 +0200

    make attributes accessible only if protected_attributes lib is included (not the case in rails 4 by default)

--- lib/acts_as_translatable/record_translation.rb
+++ lib/acts_as_translatable/record_translation.rb
@@ -1,4 +1,4 @@
 # class to hold record translations
 class RecordTranslation < ActiveRecord::Base
-  attr_accessible :translatable_id, :translatable_type, :translatable_field, :locale, :content
+  attr_accessible :translatable_id, :translatable_type, :translatable_field, :locale, :content if respond_to? :attr_accessible
 end
