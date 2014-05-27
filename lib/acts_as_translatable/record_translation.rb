d47fcda2754a51ea430d33f3a2ae0b7eea9e10c4
Andre Winschu <aw@megorei.com>
2014-05-27 10:14:55 +0200

    changed to polymorphic associations

--- lib/acts_as_translatable/record_translation.rb
+++ lib/acts_as_translatable/record_translation.rb
@@ -1,4 +1,5 @@
 # class to hold record translations
 class RecordTranslation < ActiveRecord::Base
+  belongs_to :translatable, :polymorphic => true
   attr_accessible :translatable_id, :translatable_type, :translatable_field, :locale, :content if respond_to? :attr_accessible
 end
